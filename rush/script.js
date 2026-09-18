fcb8d2b39f888e1b5f69604eda76b2a91e58ece8
Tanadol Kamthong <tanadolkamthong@gmail.com>
2026-09-18 18:24:05 +0700

    rush

--- rush/script.js
+++ rush/script.js
@@ -1,13 +1,10 @@
-// โค้ด JS ง่ายๆ สำหรับทำ Smooth Scrolling เวลากดที่เมนู Navbar
 document.querySelectorAll('.nav-links a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-        e.preventDefault(); // ปิดการกระโดดแบบปกติ
+        e.preventDefault();
 
-        // ดึง id ของส่วนที่เราต้องการเลื่อนไป
         const targetId = this.getAttribute('href').substring(1);
         const targetElement = document.getElementById(targetId);
 
-        // สั่งให้เลื่อนหน้าจออย่างสมูท
         window.scrollTo({
             top: targetElement.offsetTop,
             behavior: 'smooth'
